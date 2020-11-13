85c6e29cc0d16bead360af0ad16773d3d293e0ed
clement880101 <clement880101@gmail.com>
2020-11-13 01:32:43 -0600

    Added new indices for get_daily_technical
    Updated Documentation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # MLStocks
+
+**get_daily_ts(stock, API_key):**<br />
+Returns daily opening, closing, highest, 
+and lowest price, as well as daily volume.<br /><br />
+
+**get_daily_technical(stock, indicator, API_key, *period*):**<br />
+Returns data for different types of indicators. Period is an optional
+variable specifying period of calculation if applicable.<br /><br />
+Supported indicators include:
+- 'bband'; Bollinger Band; Default period = 20
+- 'macd'; Moving Average Convergence Divergence; Period not in use
+- 'rsi'; Relative Strength Index; Default period = 14
+- 'cci'; Commodity Channel Index; Default period = 20
+- 'aroon'; Aroon; Default period = 14
--- main.py
+++ main.py
@@ -12,28 +12,49 @@ def get_daily_ts(stock, API_key):
     return data, meta_data
 
 
-def get_daily_technical(stock, indicator, API_key, period=20):
+def get_daily_technical(stock, indicator, API_key, period=-1):
     ti = TechIndicators(key=API_key, output_format='pandas')
     if indicator == "bband":
         # Return daily bband with 20 sma
+        if(period <= 0):
+            period = 20
         data, meta_data = ti.get_bbands(symbol=stock, interval='daily', time_period=period)
     elif indicator == "macd":
         # Return daily macd
         data, meta_data = ti.get_macd(symbol=stock, interval='daily')
-    elif indicator == "macd":
-        # Return daily macd
-        data, meta_data = ti.get_macd(symbol=stock, interval='daily')
+    elif indicator == "rsi":
+        # Return daily rsi
+        if (period <= 0):
+            period = 14
+        data, meta_data = ti.get_rsi(symbol=stock, interval='daily', time_period=period)
+    elif indicator == "cci":
+        # Return daily rsi
+        if (period <= 0):
+            period = 20
+        data, meta_data = ti.get_cci(symbol=stock, interval='daily', time_period=period)
+    elif indicator == "aroon":
+        # Return daily rsi
+        if (period <= 0):
+            period = 14
+        data, meta_data = ti.get_aroon(symbol=stock, interval='daily', time_period=period)
     else:
         sys.exit('Failed to input a valid indicator')
 
     return data, meta_data
 
+def merge(left, right):
+    left.join(right, how='inner')
+    return left
 
 def main():
     API_key = 'ATFBYYKDGZGWGJXS'
-    googl_data, googl_meta = get_daily_ts('Googl', API_key)
-    print(googl_data)
-    print(googl_meta)
+    data1, meta1 = get_daily_technical('googl', 'macd', API_key)
+    data2, meta2 = get_daily_technical('googl', 'bband', API_key)
+    print(data1)
+    print(data2)
+    data = merge(data1, data2)
+    print(data)
+
 
 
 if __name__ == "__main__":
