058355e68b85084fe573fc4e6d097cefe92c34c9
Haniel Paulos <hpaulos@emory.edu>
2020-11-12 21:26:15 -0500

    nov 12 9PM

--- main.py
+++ main.py
@@ -6,27 +6,34 @@ from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 
 
+# for installing alpha vantage package
 def install(package):
     import subprocess
     import sys
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
-def get_daily_ts(stock, API_key):
-    # Return daily closing price
+def get_daily_ts(stock, type, API_key):
+    # Return daily type of historical data
     ts = TimeSeries(key=API_key, output_format='pandas')
     data, meta_data = ts.get_daily(stock)
-    return data, meta_data
+    data.columns = ['open', 'high', 'low', 'close', 'volume']
+
+    return data[type]
 
 
-def get_daily_technical(stock, indicator, API_key):
+def get_daily_technical(stock, indicator, API_key, period=20):
     ti = TechIndicators(key=API_key, output_format='pandas')
     if indicator == "bband":
         # Return daily bband with 20 sma
-        data, meta_data = ti.get_bbands(symbol=stock, interval='daily', time_period=20)
+        data, meta_data = ti.get_bbands(symbol=stock, interval='daily', time_period=period)
     elif indicator == "macd":
-        # Return daily bband with 20 sma
-        data, meta_data = ti.get_macd(symbol=stock, interval='daily', )
+        # Return daily macd
+        data, meta_data = ti.get_macd(symbol=stock, interval='daily')
+    elif indicator == "macd":
+        # Return daily macd
+        data, meta_data = ti.get_macd(symbol=stock, interval='daily')
+
     return data, meta_data
 
 
