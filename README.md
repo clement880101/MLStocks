bb6ceafad6bd1dcb62fe34861e4dcc018c5a0869
clement880101 <clement880101@gmail.com>
2020-11-15 14:03:43 -0600

    Added new functions and update readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # MLStocks
 
+##compile.py 
+For compiling daily data
 ### Alpha Vantage Related Functions
 **get_daily_ts(stock, API_key):**<br />
 Returns daily opening, closing, highest, 
@@ -10,19 +12,23 @@ Returns data for different types of indicators. Period is an optional
 variable specifying period of calculation if applicable.<br /><br />
 Supported indicators include:
 - 'bband'; Bollinger Band; Default period = 20
-- 'macd'; Moving Average Convergence Divergence; Period not in use
+- 'macd'; Moving Average Convergence Divergence; Period not used
 - 'rsi'; Relative Strength Index; Default period = 14
 - 'cci'; Commodity Channel Index; Default period = 20
-- 'aroon'; Aroon; Default period = 14
+- 'aroon'; Aroon; Default period = 14; Period not used
+- 'ad'; Chaikin A/D line; 
 
 ### Other Functions
 **merge(left, right)**<br />
 Combine two dataframes<br />
 
 **compile(stock)** Incomplete<br />
-Combine all data we want on the stock 
-and save it as stockname.csv
+Combine all data we want on a stock 
+and return a dataframe. A csv file will also
+be created.
 
-**create_df(stock_names, stocks_data)**<br />
-Adds stock name as a prefix to all columns in each stock dataframe 
-and combines dataframes together on date. 
+**combine_df(stock_names, stocks_data)**<br />
+Merge dataframe of different stocks together to
+Combined_Stock_Data.csv
+
+##rnn (Recurrent Neural Network)
---
+++ compile.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import sys
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
+import time
+
+
+def get_daily_ts(stock, API_key):
+    # Return daily type of historical data
+    ts = TimeSeries(key=API_key, output_format='pandas')
+    data, meta_data = ts.get_daily(stock, outputsize="full")
+    data.columns = ['open', 'high', 'low', 'close', 'volume']
+    return data, meta_data
+
+
+def get_daily_technical(stock, indicator, API_key, period=-1):
+    ti = TechIndicators(key=API_key, output_format='pandas')
+    if indicator == "bband":
+        if (period <= 0):
+            period = 20
+        data, meta_data = ti.get_bbands(symbol=stock, interval='daily', time_period=period)
+    elif indicator == "macd":
+        data, meta_data = ti.get_macd(symbol=stock, interval='daily')
+    elif indicator == "rsi":
+        if (period <= 0):
+            period = 14
+        data, meta_data = ti.get_rsi(symbol=stock, interval='daily', time_period=period)
+    elif indicator == "cci":
+        if (period <= 0):
+            period = 20
+        data, meta_data = ti.get_cci(symbol=stock, interval='daily', time_period=period)
+    elif indicator == "aroon":
+        if (period <= 0):
+            period = 14
+        data, meta_data = ti.get_aroon(symbol=stock, interval='daily', time_period=period)
+    elif indicator == "ad":
+        data, meta_data = ti.get_ad(symbol=stock, interval='daily')
+    else:
+        sys.exit('Failed to input a valid indicator')
+
+    return data, meta_data
+
+
+def merge(left, right):
+    return pd.concat(objs=[left, right], join="inner", axis=1)
+
+
+def compile(stock):
+    # Keys: ##ATFBYYKDGZGWGJXS##, ##AUV1J66PW0AGIHP3##
+    API_key = 'AUV1J66PW0AGIHP3'
+
+    print("Waiting for 1 minute cooldown")
+    time.sleep(60)
+
+    print("Start Compiling...")
+    comp, meta = get_daily_ts(stock, API_key)
+    data, meta = get_daily_technical(stock, 'bband', API_key, 20)
+    # print(data)
+    comp = merge(comp, data)
+    data, meta = get_daily_technical(stock, 'macd', API_key)
+    # print(data)
+    comp = merge(comp, data)
+    data, meta = get_daily_technical(stock, 'rsi', API_key, 14)
+    # print(data)
+    comp = merge(comp, data)
+    data, meta = get_daily_technical(stock, 'cci', API_key, 20)
+    # print(data)
+    comp = merge(comp, data)
+    # Alpha Vantage only allows 5 pulls per minute
+    print("Waiting for 1 minute cooldown")
+    time.sleep(60)
+
+    data, meta = get_daily_technical(stock, 'aroon', API_key, 14)
+    comp = merge(comp, data)
+
+
+    # Add more data here
+
+
+    comp = comp.add_prefix(stock + '_')
+    data, meta = get_daily_ts('SPX', API_key)
+    print('Done with stock: ' + stock)
+    merge(comp, data).to_csv(stock + ".csv")
+    return comp
+
+
+def combine_df(stocks_names, stocks_data):
+    # Keys: ##ATFBYYKDGZGWGJXS##, ##AUV1J66PW0AGIHP3##
+    API_key = 'AUV1J66PW0AGIHP3'
+    df = stocks_data[0]
+    if len(stocks_names) > 1:
+        # we join each stock to the df by inner and on the date axis
+        for x in range(1, len(stocks_names)):
+            df = merge(df, stocks_data[x])
+    # Put SP500 data into df
+    data, meta = get_daily_ts('SPX', API_key)
+    df = merge(df, data)
+    df.to_csv("Combined_Stock_Data.csv")
+    return df
+
+
+def main():
+    stocks_names = ['Aapl', 'Googl']
+    stocks_data = []
+    for stock in stocks_names:
+        stocks_data.append(compile(stock))
+    combine_df(stocks_names, stocks_data)
+
+
+if __name__ == "__main__":
+    main()
